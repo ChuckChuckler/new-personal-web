ae931f9073a95766f4901b3b02aa55dfab5841e0
Avishi Sharma <robloxpusheen8@gmail.com>
2025-11-22 22:06:27 -0500

    work on about me page

--- scripts/aboutMe.js
+++ scripts/aboutMe.js
@@ -9,3 +9,60 @@ function randint(min, max){
 function rdrct(link){
     window.location.href = link;
 }
+
+window.onload = function(){
+    new TypeIt(".about-me-title",{
+        speed: 80,
+        loop: false
+    }).go();
+}
+
+
+
+//gsap animations
+
+let animRunning = false;
+
+gsap.registerPlugin(ScrollTrigger,ScrollToPlugin);
+
+ScrollTrigger.create({
+    trigger: ".about-me-text",
+    start: "top bottom",
+    end: "bottom top",
+    onEnter:()=>{
+        if(!animRunning){
+            animRunning = true;
+            let scroller = gsap.timeline();
+            scroller.to(window,{
+                duration: 1.5,
+                scrollTo: ".about-me-text",
+                ease: "power3.out"
+            });
+            wait(1500).then(()=>{
+                animRunning = false;
+            });
+        }
+    }
+});
+
+ScrollTrigger.create({
+    trigger: ".plaid-bg",
+    start: "bottom top",
+    end: "bottom bottom",
+    onUpdate:(self)=>{
+        if(self.direction==-1){
+            if(!animRunning){
+                animRunning = true;
+                let scroller = gsap.timeline();
+                scroller.to(window,{
+                    duration: 1.5,
+                    scrollTo: ".plaid-bg",
+                    ease: "power3.out"
+                });
+                wait(1500).then(()=>{
+                    animRunning = false;
+                });
+            }
+        }
+    }
+});
