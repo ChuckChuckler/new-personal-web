d86cab4cfe9094017f9a12b60cbe16d402456933
Avishi Sharma <robloxpusheen8@gmail.com>
2025-11-23 13:02:09 -0500

    animations on about me page

--- scripts/aboutMe.js
+++ scripts/aboutMe.js
@@ -37,7 +37,17 @@ ScrollTrigger.create({
                 duration: 1.5,
                 scrollTo: ".about-me-text",
                 ease: "power3.out"
-            });
+            })
+            scroller.to(".img-holder",{
+                duration: 1.5,
+                right: 0,
+                ease: "power3.out"
+            }, "<")
+            scroller.to(".about-me-info",{
+                duration: 1.5,
+                left: 0,
+                ease: "power3.out"
+            },"<")
             wait(1500).then(()=>{
                 animRunning = false;
             });
@@ -58,7 +68,107 @@ ScrollTrigger.create({
                     duration: 1.5,
                     scrollTo: ".plaid-bg",
                     ease: "power3.out"
+                })
+                scroller.to(".img-holder",{
+                    duration: 1.5,
+                    right: "400px",
+                    ease: "power3.out"
+                },"<")
+                scroller.to(".about-me-info",{
+                    duration: 1.5,
+                    left: "1000px",
+                    ease: "power3.out"
+                },"<")
+                wait(1500).then(()=>{
+                    animRunning = false;
                 });
+            }
+        }
+    }
+});
+
+ScrollTrigger.create({
+    trigger: ".hobbies",
+    start: "top bottom",
+    end: "bottom top",
+    onEnter:()=>{
+        if(!animRunning){
+            animRunning = true;
+            let scroller = gsap.timeline();
+            scroller.to(window,{
+                duration: 1.5,
+                scrollTo: ".hobbies",
+                ease: "power3.out"
+            })
+            scroller.to(".img-holder",{
+                duration: 1.5,
+                right: "400px",
+                ease: "power3.out"
+            },"<")
+            scroller.to(".about-me-info",{
+                duration: 1.5,
+                left: "1000px",
+                ease: "power3.out"
+            },"<")
+            scroller.to(".hobbies-divider",{
+                duration: 1.5,
+                width: "35%",
+                ease: "power3.out"
+            },"<")
+            scroller.to(".hobbies-holder",{
+                duration: 1.5,
+                width: "90%",
+                ease: "power3.out"
+            },"<")
+            scroller.to(".hobby",{
+                duration: 1.5,
+                opacity: 1
+            },"<")
+            wait(1500).then(()=>{
+                animRunning = false;
+            });
+        }
+    }
+});
+
+ScrollTrigger.create({
+    trigger: ".about-me-text",
+    start: "bottom top",
+    end: "bottom bottom",
+    onUpdate:(self)=>{
+        if(self.direction==-1){
+            if(!animRunning){
+                animRunning = true;
+                let scroller = gsap.timeline();
+                scroller.to(window,{
+                    duration: 1.5,
+                    scrollTo: ".about-me-text",
+                    ease: "power3.out"
+                })
+                scroller.to(".img-holder",{
+                    duration: 1.5,
+                    right: 0,
+                    ease: "power3.out"
+                },"<")
+                scroller.to(".about-me-info",{
+                    duration: 1.5,
+                    left: 0,
+                    ease: "power3.out"
+                },"<")
+                scroller.to(".hobbies-divider",{
+                    duration: 1.5,
+                    width: "0%",
+                    ease: "power3.out"
+                },"<")
+                scroller.to(".hobbies-holder",{
+                    duration: 1.5,
+                    width: "0%",
+                    ease: "power3.out"
+                },"<")
+                scroller.to(".hobby",{
+                    duration: 1.5,
+                    opacity: 1
+                },"<")
                 wait(1500).then(()=>{
                     animRunning = false;
                 });
