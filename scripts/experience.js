dce09bfc7e567558469f43575fbd0c1a86b953f0
Avishi Sharma <robloxpusheen8@gmail.com>
2025-11-25 09:25:12 -0500

    work on experience page

---
+++ scripts/experience.js
@@ -0,0 +1,11 @@
+function wait(ms){
+    return new Promise(resolve=>setTimeout(resolve, ms));
+}
+
+function randint(min, max){
+    return Math.floor((Math.random()*max-min)+min);
+}
+
+function rdrct(link){
+    window.location.href = link;
+}
