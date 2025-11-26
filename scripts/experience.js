fb132320a10f587b04d57abd2a3a11cb7a584fc0
Avishi Sharma <robloxpusheen8@gmail.com>
2025-11-26 16:09:30 -0500

    Experience page basic animations, scroll

--- scripts/experience.js
+++ scripts/experience.js
@@ -10,6 +10,10 @@ function rdrct(link){
     window.location.href = link;
 }
 
+function newTabRdrct(link){
+    window.open(link, "_blank");
+}
+
 function hover(text){
     document.getElementById("skill").innerText = text;
 }
@@ -18,5 +22,92 @@ function hover(text){
 
 //gsap animations
 
-//gsap.registerPlugin()
+
+let animRunning = false;
+
+gsap.registerPlugin(ScrollTrigger,ScrollToPlugin);
+
+ScrollTrigger.create({
+    trigger: ".skills",
+    start: "top bottom",
+    end: "bottom top",
+    onEnter:()=>{
+        if(!animRunning){
+            animRunning = true;
+            let scroller = gsap.timeline();
+            scroller.to(window,{
+                duration: 1.5,
+                scrollTo: ".skills",
+                ease: "power3.out"
+            })
+            wait(1500).then(()=>{
+                animRunning = false;
+            });
+        }
+    }
+});
+
+ScrollTrigger.create({
+    trigger: ".plaid-bg",
+    start: "bottom top",
+    end: "bottom bottom",
+    onUpdate:(self)=>{
+        if(self.direction==-1){
+            if(!animRunning){
+                animRunning = true;
+                let scroller = gsap.timeline();
+                scroller.to(window,{
+                    duration: 1.5,
+                    scrollTo: ".plaid-bg",
+                    ease: "power3.out"
+                })
+                wait(1500).then(()=>{
+                    animRunning = false;
+                });
+            }
+        }
+    }
+});
+
+ScrollTrigger.create({
+    trigger: ".hackathon-ysws-holder",
+    start: "top bottom",
+    end: "bottom top",
+    onEnter:()=>{
+        if(!animRunning){
+            animRunning = true;
+            let scroller = gsap.timeline();
+            scroller.to(window,{
+                duration: 1.5,
+                scrollTo: ".hackathon-ysws-holder",
+                ease: "power3.out"
+            })
+            wait(1500).then(()=>{
+                animRunning = false;
+            });
+        }
+    }
+});
+
+ScrollTrigger.create({
+    trigger: ".skills",
+    start: "bottom top",
+    end: "bottom bottom",
+    onUpdate:(self)=>{
+        if(self.direction==-1){
+            if(!animRunning){
+                animRunning = true;
+                let scroller = gsap.timeline();
+                scroller.to(window,{
+                    duration: 1.5,
+                    scrollTo: ".skills",
+                    ease: "power3.out"
+                })
+                wait(1500).then(()=>{
+                    animRunning = false;
+                });
+            }
+        }
+    }
+});
 
