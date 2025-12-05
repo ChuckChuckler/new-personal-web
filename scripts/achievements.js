e99dde5a2eeaf2d6021f2b48a7b5958fd1bb633b
Avishi Sharma <robloxpusheen8@gmail.com>
2025-12-05 13:47:37 -0500

    Finish-up of achievements page

---
+++ scripts/achievements.js
@@ -0,0 +1,15 @@
+function wait(ms){
+    return new Promise(resolve=>setTimeout(resolve, ms));
+}
+
+function randint(min, max){
+    return Math.floor((Math.random()*max-min)+min);
+}
+
+function rdrct(link){
+    window.location.href = link;
+}
+
+function newTabRdrct(link){
+    window.open(link, "_blank");
+}
